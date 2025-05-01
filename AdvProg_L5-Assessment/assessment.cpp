d1b62b8223b344acd0d05b6585add8390851484e
hsrassiw <minhnghianshd@gmail.com>
2025-05-01 21:47:46 +0700

    update

--- AdvProg_L5-Assessment/assessment.cpp
+++ AdvProg_L5-Assessment/assessment.cpp
@@ -1,5 +1,6 @@
 #include "assessment.h"
 
+
 using std::string;
 using std::vector;
 
@@ -19,6 +20,11 @@ void updateMask(string& mask, const char ch, const string& word)
           ch (const char): the character that is tested to see if it is in the word or not
           word (const string&): the word that is needed to be tested
      */
+    for (size_t i = 0; i < word.size(); ++i) {
+        if (word[i] == ch) {
+            mask[i] = ch;
+        }
+    }
 
 }
 
@@ -34,6 +40,34 @@ int countMistakes(const string& word, HangmanGuesser& hmGuesser)
      Returns:
          int: the number of mistakes made by the guesser
      */
+    string mask(word.size(), '-');
+    int mistakeCount = 0;
+
+    hmGuesser.newGame(word.size(), '-');
+
+    while (mask != word) {
+        char ch = hmGuesser.getChar(mask);
+        if (ch == 0 || mask.find(ch) != string::npos) {
+            break;
+        }
+
+        bool correct = false;
+        for (int i = 0; i < word.size(); ++i) {
+            if (word[i] == ch) {
+                mask[i] = ch;
+                correct = true;
+            }
+        }
+
+        if (!correct) {
+            ++mistakeCount;
+        }
+
+        if (mask == word) {
+            return mistakeCount;
+        }
+    }
+
     return -1;
 }
 
@@ -50,6 +84,14 @@ void getMistakeByWordList(vector<MistakeByWord>& mistakeList, vector<string>& un
          testWords (vector<string>&): the list of words that need to be tested
          hmGuesser (HangmanGuesser&): the guesser that is used to make the guess
      */
+    for (const auto& word : testWords) {
+        int m = countMistakes(word, hmGuesser);
+        if (m < 0) {
+            unsolvableList.push_back(word);
+        } else {
+            mistakeList.emplace_back(MistakeByWord{m, word});
+        }
+    }
 }
 
 bool isCorrectChar(char ch, const string& mask)
